1ac6d7ce1874e8a7462a7e6e563b72c7bf091406
tumee <tmkee0525@gmail.com>
2025-02-19 18:41:49 +0900

    added register

---
+++ app/Filament/Resources/RegisterResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RegisterResource\Pages;
+use App\Filament\Resources\RegisterResource\RelationManagers;
+use App\Models\Register;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class RegisterResource extends Resource
+{
+    protected static ?string $model = Register::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRegisters::route('/'),
+            'create' => Pages\CreateRegister::route('/create'),
+            'edit' => Pages\EditRegister::route('/{record}/edit'),
+        ];
+    }
+}
